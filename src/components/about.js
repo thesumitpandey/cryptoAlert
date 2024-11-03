7226128cd9155d69c336f3c2456fcb475444560b
thesumitpandey <128164065+thesumitpandey@users.noreply.github.com>
2024-11-03 22:37:41 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -2,18 +2,23 @@ import React from 'react'
 import Accordion from 'react-bootstrap/Accordion';
 import  "./file.css";
 
+import Card from 'react-bootstrap/Card';
+
+
+
 
 
 
 const about=()=> {
   return (
-    <div >
-        <div className="container " style={{padding:100, minHeight:900}}>
-
-        <Accordion defaultActiveKey="0"     >
-      <Accordion.Item eventKey="0" >
-        <Accordion.Header  ><strong>About us</strong></Accordion.Header>
-        <Accordion.Body className='head'>
+    <div className="mi"> 
+    <Accordion defaultActiveKey="0" className='contain a'>
+    <Card className=''>
+      <Card.Header>
+      
+      </Card.Header>
+      <Accordion.Collapse eventKey="0">
+        <Card.Body className='head'>      
         Welcome to CryptoAlert, your go-to app for real-time updates and insights into the dynamic world of cryptocurrencies. Our platform provides you with the latest information on major cryptocurrencies, including:
         Bitcoin (BTC)
         Ethereum (ETH)
@@ -26,14 +31,11 @@ At CryptoAlert, our mission is to empower users with timely updates and comprehe
 
         Thank you for choosing  CryptoAlert as your trusted news source. We look forward to keeping you informed!
 </h5>
-
-        </Accordion.Body>
-      </Accordion.Item>
-      
-    </Accordion>
-        </div>
-
-    </div>
+</Card.Body>
+      </Accordion.Collapse>
+    </Card>
+  </Accordion>
+  </div>
   )
 }
 export default about;
